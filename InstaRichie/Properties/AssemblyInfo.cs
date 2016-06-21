cc632d443861345c53e2e9ec987e96ce4e11c289
Jijo Bose <jijobose@msn.com>
2016-06-21 18:33:38 +0530

    Re Branding the App

--- InstaRichie/Properties/AssemblyInfo.cs
+++ InstaRichie/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("InstaRichie")]
+[assembly: AssemblyTitle("FeatWallet")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("InstaRichie")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("FeatWallet")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
