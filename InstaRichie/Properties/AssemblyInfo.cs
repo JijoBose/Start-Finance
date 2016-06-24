42cbe922eb03670296f05c30a38ee41b55783862
Jijo Bose <jijobose@msn.com>
2016-06-24 16:16:47 +0530

    Re-Branded to JustWallet
    from FeatWallet

--- InstaRichie/Properties/AssemblyInfo.cs
+++ InstaRichie/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FeatWallet")]
+[assembly: AssemblyTitle("JustWallet")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FeatWallet")]
+[assembly: AssemblyProduct("JustWallet")]
 [assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
